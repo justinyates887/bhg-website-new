d6da389660c24384ea266b3b9a498ed1b56a019b
Justin Yates <justinyates887@gmail.com>
2021-05-05 14:04:20 -0700

    added chakra ui, started react design

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,4 +8,12 @@ router.get('/discord/redirect', passport.authenticate('discord'), (req, res) => 
     res.send(200)
 })
 
+router.get('/', (req, res) => {
+    if (req.user){
+        res.send(req.user)
+    } else {
+        res.status(401).send({ msg: 'Unauthorized '})
+    }
+})
+
 module.exports = router
