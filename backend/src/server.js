32dd90b18f6ad97e7edf41d49cb890466a3b92e7
Justin Yates <justinyates887@gmail.com>
2021-05-24 15:21:51 -0700

    asdasdad

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,8 +14,6 @@ const PORT = process.env.PORT || 8081;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
-
 
 mongoose.connect(process.env.URI,
     {
